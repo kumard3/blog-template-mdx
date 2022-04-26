7053d13a1cc3e3f6df7e721f652664feaa2ee6f7
kumard3 <kumardeepanshu157200@gmail.com>
2022-04-27 02:35:43 +0530

    hero section

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,7 @@
 export default function Layout({ children }: any) {
   return (
     <>
-      <div className="bg-black min-h-screen text-white">{children}</div>
+      <div className="">{children}</div>
     </>
   )
 }
